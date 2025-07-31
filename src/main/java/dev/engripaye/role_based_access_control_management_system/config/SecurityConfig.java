b9903438f55a331ef5c31750ed9f6a14d2ba4aef
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-31 12:24:58 +0100

    "database" commit by Engr. Ipaye

---
+++ src/main/java/dev/engripaye/role_based_access_control_management_system/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package dev.engripaye.role_based_access_control_management_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    // We’ll configure Spring Security to use an in-memory user store for
+    // simplicity and enforce RBAC using @PreAuthorize.
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth .requestMatchers("/api/public/**").permitAll()
+                                .anyRequest().authenticated()
+                        )
+                .httpBasic(withDefaults()); // Optional: import static org.springframework.security.config.Customizer.withDefaults()
+
+        return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user")
+                .password("{noop}userpass")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminopass")
+                .roles("ADMIN, USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+
+    }
+}
---
+++ src/main/java/dev/engripaye/role_based_access_control_management_system/controller/ApiController.java
@@ -0,0 +1,32 @@
+package dev.engripaye.role_based_access_control_management_system.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/public/hello")
+    public String publicHello(){
+
+        return "Hello, User!";
+    }
+
+    @GetMapping("user/greeting")
+    @PreAuthorize("hasRole('USER')")
+    public String userGreeting(){
+
+        return "Hello User, Access has been granted!";
+
+    }
+
+    @GetMapping("admin/greeting")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminGreeting(){
+        return "Welcome to the Admin Dashboard";
+    }
+
+}
