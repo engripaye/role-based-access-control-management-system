2601ee262bc9ce53a6f966cc3cc2aa2908facd2c
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-31 11:03:03 +0100

    init commit

---
+++ src/main/java/dev/engripaye/role_based_access_control_management_system/RoleBasedAccessControlManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.role_based_access_control_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAccessControlManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAccessControlManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engripaye/role_based_access_control_management_system/RoleBasedAccessControlManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.role_based_access_control_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAccessControlManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
